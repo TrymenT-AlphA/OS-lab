83b78872bfc1677c9cebec58b30b37f589cd6de7
ChongKai <1302393176@qq.com>
2022-06-23 19:08:22 +0800

    改写file_read，将文件数据读入内核缓冲区

--- fs/file_dev.c
+++ fs/file_dev.c
@@ -45,6 +45,39 @@ int file_read(struct m_inode * inode, struct file * filp, char * buf, int count)
 	return (count-left)?(count-left):-ERROR;
 }
 
+// st ChongKai
+int ck_file_read(struct m_inode * inode, struct file * filp, char * buf, int count)
+{
+	int left,chars,nr;
+	struct buffer_head * bh;
+
+	if ((left=count)<=0)
+		return 0;
+	while (left) {
+		if (nr = bmap(inode,(filp->f_pos)/BLOCK_SIZE)) {
+			if (!(bh=bread(inode->i_dev,nr)))
+				break;
+		} else
+			bh = NULL;
+		nr = filp->f_pos % BLOCK_SIZE;
+		chars = MIN( BLOCK_SIZE-nr , left );
+		filp->f_pos += chars;
+		left -= chars;
+		if (bh) {
+			char * p = nr + bh->b_data;
+			while (chars-->0)
+				*(buf++) = *(p++);
+			brelse(bh);
+		} else {
+			while (chars-->0)
+				*(buf++) = 0;
+		}
+	}
+	inode->i_atime = CURRENT_TIME;
+	return (count-left)?(count-left):-ERROR;
+}
+// ed ChongKai
+
 int file_write(struct m_inode * inode, struct file * filp, char * buf, int count)
 {
 	off_t pos;
